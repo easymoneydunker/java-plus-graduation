f2972bf4e0b0d421e3fb776851ec68a1297ccddd
Pavel <sprt4758@gmail.com>
2025-06-02 18:33:23 +0300

    fix stat-service uri dynamic fetching

--- stats/stats-client/src/main/java/ru/practicum/ewm/client/StatClient.java
+++ stats/stats-client/src/main/java/ru/practicum/ewm/client/StatClient.java
@@ -3,6 +3,7 @@ package ru.practicum.ewm.client;
 import com.netflix.appinfo.InstanceInfo;
 import com.netflix.discovery.EurekaClient;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
@@ -16,13 +17,16 @@ import java.util.List;
 @Component
 public class StatClient {
     private final EurekaClient eurekaClient;
+    private final String statServerName;
 
-    public StatClient(EurekaClient eurekaClient) {
+    public StatClient(EurekaClient eurekaClient,
+                      @Value("${stat-server.eureka-name:stats-server}") String statServerName) {
         this.eurekaClient = eurekaClient;
+        this.statServerName = statServerName;
     }
 
     private URI getStatServerUri() {
-        InstanceInfo instance = eurekaClient.getNextServerFromEureka("STAT-SERVER", false);
+        InstanceInfo instance = eurekaClient.getNextServerFromEureka(statServerName, false);
         return URI.create(instance.getHomePageUrl());
     }
 
